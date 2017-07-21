2e47f98ef3ada73a2c2826438c59736f9fed66a2
Peter Karman <peter.karman@gsa.gov>
2017-07-21 17:36:10 -0500

    phptidy, plus use curl instead of file_get_contents

--- foia-proxy.php
+++ foia-proxy.php
@@ -1,37 +1,56 @@
 <?php
-    // turning off peer validation is ugly and wrong.
-    // not sure if this is a PHP CA thing, or a foia.gov cert thing.
-    $ssl_opts = array(
-        "ssl" => array(
-            "verify_peer"=>false,
-            "verify_peer_name"=>false,
-        ),
-    );
-    $base_url = 'https://www.foia.gov/';
-    if (!isset($_GET['u'])) {
-        header('X-FOIA: missing u param', 400, false);
-        print "Missing 'u' param";
-        exit;
-    }
 
-    // grab entire query string instead of using $_GET because
-    // param value may be un-escaped and $_GET may mangle parsing.
-    $path = preg_replace('/^u=/', '', $_SERVER['QUERY_STRING']);
-    $path = urldecode($path);
+define('FOIA_BASE_URL', 'https://www.foia.gov/');
 
-    $resp = file_get_contents($base_url . $path, false, stream_context_create($ssl_opts));
-    // error_log("resp for $path: $resp");
-    if (!$resp) {
-        header('X-FOIA: bad upstream response', 404, false);
-    }
+if (!isset($_GET['u'])) {
+    header('X-FOIA: missing u param', 400, false);
+    print "Missing 'u' param";
+    exit;
+}
 
-    $filtered = filter_response($resp);
-    // error_log("filtered: $filtered");
-    print $filtered;
+// grab entire query string instead of using $_GET because
+// param value may be un-escaped and $_GET may mangle parsing.
+$path = preg_replace('/^u=/', '', $_SERVER['QUERY_STRING']);
+$path = urldecode($path);
 
-    function filter_response($resp) {
-        $resp = preg_replace('/\/FusionCharts\//', './FusionCharts/', $resp);
-        $resp = preg_replace('/\/foia\/FormChart/', './foia-proxy.php?u=/foia/FormChart', $resp);
-        return $resp;
-    }
-?>
+$resp = curl_get_contents(FOIA_BASE_URL . $path);
+
+// error_log("resp for $path: $resp");
+if (!$resp) {
+    header('X-FOIA: bad upstream response', 404, false);
+}
+
+$filtered = filter_response($resp);
+// error_log("filtered: $filtered");
+print $filtered;
+
+
+/**
+ *
+ *
+ * @param string  $resp
+ * @return string
+ */
+function filter_response($resp) {
+    $resp = preg_replace('/\/FusionCharts\//', './FusionCharts/', $resp);
+    $resp = preg_replace('/\/foia\/FormChart/', './foia-proxy.php?u=/foia/FormChart', $resp);
+    return $resp;
+}
+
+
+/**
+ *
+ *
+ * @param string  $url
+ * @return string
+ */
+function curl_get_contents($url) {
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
