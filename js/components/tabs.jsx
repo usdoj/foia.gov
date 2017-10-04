13a70db2bbc9763892b372ecca54899992a189ae
Heather Battaglia <heather.battaglia@gsa.gov>
2017-10-04 02:50:50 -0500

    update colors to use new primary-alt-light color

--- js/components/tabs.jsx
+++ js/components/tabs.jsx
@@ -14,7 +14,6 @@ class Tabs extends Component {
   };
 
   handleClickTab(index, event) {
-    console.log(index);
     event.preventDefault();
     this.setState({
       selectedTab: index
@@ -22,7 +21,6 @@ class Tabs extends Component {
   }
 
   renderTabControl(name, index){
-    console.log(index);
     let key = `tab-control_${index}`;
     let className = (index == this.state.selectedTab) ? 'tab_active' : '';
     return(
@@ -52,10 +50,94 @@ class Tabs extends Component {
         </ul>
         <ul className="sidebar_tabs">
           <li className={this.state.selectedTab==0 ? 'tab_active' : ''}>
-            <p>This is the Request Directions tab.</p>
+            <h3>Department of Justice</h3>
+            <h2>Federal Bureau of Investigation</h2>
+            <section>
+              <ul className="sidebar_progress-bar">
+                <li>
+                  <a className="step_active" href="">
+                    <span>Requester contact</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <span>Request description</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <span>Supporting documentation</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <span>Processing fees</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <span>Delivery method</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <span>Submission and confirmation</span>
+                  </a>
+                </li>
+              </ul>
+            </section>
+             <section>
+              <h2>Tips for submitting</h2>
+              <ul className="submission-help_tips">
+                <li>
+                  <p>The person to reach out to about your FOIA request is:</p>
+                  <p className="submission-help_poc">Dennis Argall, Public Liason</p>
+                  <p className="submission-help_phone">540-868-4516</p>
+                  <p className="submission-help_email"><a href="mailto:foiarequest@ic.fbi.gov">foiarequest@ic.fbi.gov</a></p>
+                  <p>You can ask FOIA personnel about anything related to your request, including whether what you’re asking for is clear. You can also reach out to follow up on your request after it’s been submitted.</p>
+                </li>
+                <li>
+                  <p>The description of records you're requesting is very important.</p>
+                  <p className="submission-help_description">Be sure your request is clear and as specific as as possible.</p>
+                </li>
+                <li>
+                  <p>Do research before you file.</p>
+                  <p className="submission-help_research">Sometime records and information you’re looking for  is already public. You can find out by reaching out to the agency you’re interested in or by visiting their website or their FOIA reading room.</p>
+                </li>
+              </ul>
+            </section>
           </li>
           <li className={this.state.selectedTab==1 ? 'tab_active' : ''}>
-            <p>This is the Agency Information tab.</p>
+            <h3>Department of Justice</h3>
+            <h2>Federal Bureau of Investigation</h2>
+            <section className="submission-help_processing-time">
+              <h5>Median processing time</h5>
+              <p>15 working days for simple requests</p>
+              <p>187 working days for complex requests</p>
+            </section>
+            <section className="submission-help_agency-mission">
+              <h5>Agency mission</h5>
+              <p>As an intelligence-driven and a threat-focused national security organization with both intelligence and law enforcement responsibilities, the mission of the FBI is to protect and defend the United States against terrorist and foreign intelligence threats, to uphold and enforce the criminal laws of the United States, and to provide leadership and criminal justice services to federal, state, municipal, and international agencies and partners.</p>
+            </section>
+            <section>
+              <h5 className="submission-help_first-party-requests">First party requests</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <h5 className="submission-help_expedited processing">Expedited processing</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </section>
+            <section className="submission-help_contact">
+              <h5>Contact</h5>
+              <p className="submission-help_website">fbi.gov</p>
+              <p className="submission-help_poc">Dennis Argall, Public Liason</p>
+              <p className="submission-help_phone">540-868-4516</p>
+              <p className="submission-help_email"><a href="mailto:foiarequest@ic.fbi.gov">foiarequest@ic.fbi.gov</a></p>
+              <address className="submission-help_mailing">
+                <p>David M. Hardy</p>
+                <p>Chief, Record/Information Dissemination Section, Records Management Division</p>
+                <p>170 Marcel Drive</p>
+                <p>Winchester, VA 22602-4843</p>
+              </address>
+            </section>
           </li>
         </ul>
       </div>
