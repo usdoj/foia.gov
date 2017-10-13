465435b8d033a8f8e874bc00c8061f0f79d346b2
Aaron D Borden <adborden@a14n.net>
2017-10-12 19:29:33 -0700

    Refactor ProgressBar

---
+++ js/components/progress_bar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function href(id) {
+  // ids used by react-jsonschema-form
+  return `#root_${id}__title`;
+}
+
+function ProgressBar({ sections }) {
+  return (
+    <ul className="sidebar_progress-bar">
+      {
+        sections.map(section => (
+          <li key={section.id}>
+            <a href={href(section.id)}>
+              <span>{section.title}</span>
+            </a>
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+ProgressBar.propTypes = {
+  sections: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+
+export default ProgressBar;
