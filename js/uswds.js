27d59459b429f0ff33e6987238756e861f0f1653
Heather Battaglia <heather.battaglia@gsa.gov>
2017-10-26 21:52:17 -0600

    shorten comment for linter

--- js/uswds.js
+++ js/uswds.js
@@ -1,2 +1,3 @@
-// There's an issue importing uswds, the minified file, so instead import the source and minify it ourselves in webpack
+// There's an issue importing uswds, the minified file,
+// so instead import the source and minify it ourselves in webpack
 import 'uswds/dist/js/uswds';
