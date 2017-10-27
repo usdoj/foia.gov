c36353ffd7f2cad39498a600ef9039ac0a787c86
Aaron D Borden <adborden@a14n.net>
2017-10-26 18:31:20 -0700

    Add local_api
    Use our stub API strategy to put content in yaml/markdown files and serve them
    as json over an API.

--- js/settings/cloud-gov.js
+++ js/settings/cloud-gov.js
@@ -1,4 +1,5 @@
 const api = {
+  localApiBaseURL: '/api',
   jsonApiBaseURL: 'https://dev-api.foia.gov/api',
   requestApiBaseURL: 'https://dev-api.foia.gov/api',
   // These are not secret, refer to https://github.com/18F/beta.foia.gov/tree/develop/docs/foia-api.md
--- js/settings/development.js
+++ js/settings/development.js
@@ -1,4 +1,5 @@
 const api = {
+  localApiBaseURL: '/api',
   jsonApiBaseURL: 'http://foiadev.prod.acquia-sites.com/api',
   requestApiBaseURL: 'http://foiadev.prod.acquia-sites.com/api',
   // These are not secret, refer to https://github.com/18F/beta.foia.gov/tree/develop/docs/foia-api.md
