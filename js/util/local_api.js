4debfed2ffdc2915124a61a81a40f4971e00cf2e
Aaron D Borden <adborden@a14n.net>
2017-10-26 18:31:20 -0700

    Add local_api
    Use our stub API strategy to put content in yaml/markdown files and serve them
    as json over an API.

---
+++ js/util/local_api.js
@@ -0,0 +1,32 @@
+/*
+ * LocalApi
+ *
+ * This api requests static files from the front end. Allows us to have content
+ * editors write in yaml/markdown but have the data served as json.
+ */
+import axios from 'axios';
+import settings from 'settings';
+
+class LocalApi {
+  constructor(baseURL) {
+    this._api = axios.create({
+      baseURL: baseURL || settings.api.localApiBaseURL,
+    });
+  }
+
+  get(...args) {
+    return this._api.get(...args)
+      .then(response => response.data);
+  }
+
+  glossary() {
+    return this.get('/glossary/terms.json');
+  }
+}
+
+const localApi = new LocalApi();
+export default localApi;
+
+export {
+  LocalApi,
+};
