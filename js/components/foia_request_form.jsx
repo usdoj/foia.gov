64c57b5e842340ac675b2c7265bbb5867541265c
Heather Battaglia <heather.battaglia@gsa.gov>
2017-11-03 22:30:34 -0600

    style submission

--- js/components/foia_request_form.jsx
+++ js/components/foia_request_form.jsx
@@ -57,13 +57,19 @@ function FoiaRequestForm({ formData, isSubmitting, onSubmit, requestForm, submis
       widgets={widgets}
     >
       <div id="foia-request-form_submit" className="foia-request-form_submit">
-        <p>Please review the information you’ve entered and submit.</p>
-        <p>
-          You should hear from the agency you’ve submitted a records request to
-          within 10 days. If you don’t hear from the agency, please reach out
-          using the contact information provided to you on this site.
-        </p>
-        <button type="submit">Submit</button>
+        <div className="foia-request-form_inline-progress">
+          Step 6 of 6
+        </div>
+        <h4>Submission and confirmation</h4>
+        <div className="info-box">
+          <p>Please review the information you’ve entered and submit.</p>
+          <p>
+            You should hear from the agency you’ve submitted a records request to
+            within 10 days. If you don’t hear from the agency, please reach out
+            using the contact information provided to you on this site.
+          </p>
+        </div>
+        <button className="usa-button usa-button-big usa-button-primary-alt" type="submit">Submit request</button>
         { submissionResult.errorMessage &&
           <div>
             <span className="usa-input-error-message" role="alert">
