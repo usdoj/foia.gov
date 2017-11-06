f07dd8b7ab23182557701fdf4cbfa3688baf197b
Aaron D Borden <adborden@a14n.net>
2017-11-05 22:14:31 -0800

    Move request form sections to markdown
    Helps keep the site content in yaml/markdown land rather than have to edit
    javascript files.

--- split-glossary.js
+++ split-glossary.js
@@ -1,7 +1,20 @@
+/*
+ * Split the glossary and form sections into individual markdown files. This
+ * lets the content folks edit conten in markdown/yaml rather than javascript
+ * files.
+ *
+ * This script has a few additional dependencies:
+ *
+ *     $ npm install -g babel-node
+ *     $ npm install babel-preset-env
+ *     $ echo '{"presets": ["env"]}' > .babelrc
+ *     $ babel-node ./split-glossary.js
+ */
 import fs from 'fs';
 import path from 'path';
 
 import terms from './js/util/glossary/terms';
+import sections from './js/util/request_form/form_sections';
 
 function slugify(text) {
   return text.toLowerCase().replace(/[^a-z0-9-]+/g, '-');
@@ -21,5 +34,31 @@ terms.forEach((term) => {
   stream.write('---\n');
   stream.write(unindent(term.definition));
   stream.write('\n');
+  stream.end();
+});
+
+sections.forEach((section, index) => {
+  // Prefix the file with a number to keep the sort order
+  const filepath = path.join('www.foia.gov', '_request_form_sections', `0${index + 1}-${section.id}.md`);
+  console.log(filepath);
+  const stream = fs.createWriteStream(filepath);
+  stream.write('---\n');
+  stream.write(`title: "${section.title}"\n`);
+  stream.write(`_id: ${section.id}\n`);
+  if (section.isAgencySpecificFieldSection) {
+    stream.write('isAgencySpecificFieldSection: true\n');
+  }
+
+  if (!section.fieldNames.length) {
+    stream.write('fieldNames: []\n');
+  } else {
+    stream.write('fieldNames:\n');
+    section.fieldNames.forEach((fieldName) => {
+      stream.write(`- ${fieldName}\n`);
+    });
+  }
+
+  stream.write('---');
+  stream.write(unindent(section.description));
   stream.end();
 });
