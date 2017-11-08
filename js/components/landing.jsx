9a07de34d6be94fd2832922b78afeff0143c18f5
Ryan Sibley <ryannicole08@gmail.com>
2017-11-08 12:40:25 -0500

    update interoperable text
    shortening and clarifying the interoperability language that users will see.

--- js/components/landing.jsx
+++ js/components/landing.jsx
@@ -97,11 +97,7 @@ class LandingComponent extends Component {
           !this.state.agencyComponent && !this.state.agency &&
             <div>
               <h3 className="agency-component-search_hed">When choosing an agency</h3>
-              <p>Remember that some agencies have existing FOIA portals and will
-              continue to receive requests through their current portals. All
-              agencies are working towards becoming interoperable with FOIA.gov.
-              The information for where to submit a request to those agencies
-              will be available after you select an agency above.</p>
+              <p>Remember that some agencies can't yet receive FOIA requests through FOIA.gov. For those agencies, this site   will provide you with the information you need to submit a request directly to the agency.</p>
             </div>
         }
         {
