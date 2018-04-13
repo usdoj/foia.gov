d783f8def10fedd40090eb181f25ab73726c7451
Brock Fanning <brockfanning@gmail.com>
2018-04-13 20:40:28 +0000

    DOJ-610: Progress with agency contact download component.

--- js/pages/agency_component_download.jsx
+++ js/pages/agency_component_download.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Container } from 'flux/utils';
 import agencyComponentStore from '../stores/agency_component';
 import { requestActions } from 'actions';
+import ExcellentExport from 'excellentexport';
 
+/**
+ * A download page for getting all agency component contact information as CSV.
+ */
 class AgencyComponentDownloadPage extends Component {
   static getStores() {
     return [agencyComponentStore];
@@ -15,6 +19,10 @@ class AgencyComponentDownloadPage extends Component {
     };
   }
 
+  getFoiaPersonnelFields() {
+    return ['foia_officers', 'public_liaisons', 'service_centers'];
+  }
+
   componentDidMount() {
     // If there is any agency data, assume all the data is fetched.
     if (this.state.agencyComponents.size) {
@@ -22,39 +30,119 @@ class AgencyComponentDownloadPage extends Component {
     }
 
     // Pre-fetch the list of agency components, re-purposing the "finder" code.
-    const includeReferenceFields = {
-      agency_component: ['title', 'agency', 'foia_officers', 'submission_address'],
+    let includeReferenceFields = {
+      agency_component: ['title', 'agency', 'submission_address', 'submission_fax', 'website'],
       agency: ['name'],
-      foia_officers: ['name', 'title'],
     };
+    this.getFoiaPersonnelFields().forEach(personnelField => {
+      includeReferenceFields.agency_component.push(personnelField);
+      includeReferenceFields[personnelField] = ['name', 'title', 'email', 'phone'];
+    });
     requestActions.fetchAgencyFinderData(includeReferenceFields);
+  }
+
+  handleClick() {
+    if (this.rows) {
+      return ExcellentExport.convert({
+        anchor: this.myButton,
+        filename: 'FOIA-Agency-Contacts',
+        format: 'csv'
+      }, [{
+        name: 'FOIA Agency Contacts',
+        from: {
+          array: this.rows
+        }
+      }]);
+    }
   }
 
   render() {
 
     const progress = agencyComponentStore.getState().agencyFinderDataProgress;
-    if (progress >= 100) {
+
+    if (progress < 100) {
+      if (this.myButton) {
+        this.myButton.innerHTML = 'Please wait - ' + progress + '%';
+      }
+    }
+    else {
 
       const {
         agencyComponents
       } = this.state;
 
-      let tableRows = [];
-      agencyComponents.forEach(function(agency_component) {
-        agency_component.foia_officers.forEach(function(foia_officer) {
-          tableRows.push({
-            'Component': agency_component.title,
-            'Department': agency_component.agency.name,
-            'Name': foia_officer.name,
-            'Title': foia_officer.title,
+      // Construct a large array of all the FOIA personnel as rows (objects).
+      let header = [
+        'Component',
+        'Department',
+        'Name',
+        'Title',
+        'Email',
+        'Street Address 1',
+        'Street Address 2',
+        'City',
+        'State',
+        'Zip code',
+        'Telephone',
+        'Fax',
+      ];
+      let rows = [];
+      agencyComponents.forEach(agencyComponent => {
+        this.getFoiaPersonnelFields().forEach(personnelField => {
+          agencyComponent[personnelField].forEach(foiaPersonnel => {
+            let phone = '';
+            if (foiaPersonnel.phone) {
+              phone = foiaPersonnel.phone.join(', ');
+            }
+            rows.push([
+              agencyComponent.title,
+              agencyComponent.agency.name,
+              foiaPersonnel.name,
+              foiaPersonnel.title,
+              foiaPersonnel.email,
+              agencyComponent.submission_address.address_line1,
+              agencyComponent.submission_address.address_line2,
+              agencyComponent.submission_address.locality,
+              agencyComponent.submission_address.administrative_area,
+              agencyComponent.submission_address.postal_code,
+              phone,
+              agencyComponent.submission_fax,
+            ]);
           });
         });
       });
 
-      console.log(tableRows);
+      // Sort the array of rows by the agency name, then component, then contact.
+      let orderPositions = [
+        header.indexOf('Department'),
+        header.indexOf('Component'),
+        header.indexOf('Name'),
+      ];
+      rows.sort(function(a, b) {
+        for (var i in orderPositions) {
+          let pos = orderPositions[i];
+          let aLower = (a[pos]) ? a[pos].toLowerCase() : '';
+          let bLower = (b[pos]) ? b[pos].toLowerCase() : '';
+          if (aLower < bLower) {
+            return -1;
+          }
+          if (aLower > bLower) {
+            return 1;
+          }
+        }
+        return 0;
+      });
+
+      this.myButton.innerHTML = 'Download to CSV';
+      this.rows = rows;
+
     }
 
-    return null;
+    return <a
+      className="usa-button"
+      ref={c => this.myButton = c}
+      onClick={this.handleClick.bind(this)}
+    >Please wait</a>;
   }
 }
 
