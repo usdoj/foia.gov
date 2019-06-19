40b40a37c910560ff728afac3cd3cf190af4e0b5
Brock Fanning <brockfanning@gmail.com>
2019-06-19 16:17:21 +0000

    Fix link structure in tests

--- js/test/util/json_api.test.js
+++ js/test/util/json_api.test.js
@@ -164,13 +164,17 @@ describe('JsonApi', () => {
           .onFirstCall().returns(jsonApiResponse({
             data: [{ id: '1', type: 'agency', attributes: { name: 'gsa' }, links: {} }],
             links: {
-              next: '/path?offset=1',
+              next: {
+                href: '/path?offset=1',
+              },
             },
           }))
           .onSecondCall().returns(jsonApiResponse({
             data: [{ id: '2', type: 'agency', attributes: { name: 'doj' }, links: {} }],
             links: {
-              prev: '/path?offset=0',
+              prev: {
+                href: '/path?offset=0',
+              },
             },
           }));
 
