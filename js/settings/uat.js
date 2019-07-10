c6a03c3998e7ed7dbf33c4797106b907b0dc2b35
Brock Fanning <brockfanning@gmail.com>
2019-07-10 18:17:01 +0000

    DOJ-69: Settings for UAT environment.

---
+++ js/settings/uat.js
@@ -0,0 +1,12 @@
+const api = {
+  localApiBaseURL: '/api',
+  jsonApiBaseURL: 'https://uat-api.foia.gov/api',
+  requestApiBaseURL: 'https://uat-api.foia.gov/api',
+  // These are not secret, refer to https://github.com/18F/beta.foia.gov/tree/develop/docs/foia-api.md
+  jsonApiKey: 'PvO2Wh2UppBQqGIxIvdKbogNRI0W0MVLFhkxjWfK',
+};
+
+export default {
+  api,
+  appEnv: 'uat',
+};
