c8354c16b492e8d9a870afff2992687efe800789
Brock Fanning <brockfanning@gmail.com>
2021-02-19 07:43:59 -0500

    Standards tweak

--- js/settings/ddev.js
+++ js/settings/ddev.js
@@ -1,11 +1,11 @@
 const api = {
-    localApiBaseURL: '/api',
-    jsonApiBaseURL: 'https://foia-api.ddev.site/api',
-    requestApiBaseURL: 'https://foia-api.ddev.site/api',
-    jsonApiKey: 'x-local-key-not-used',
+  localApiBaseURL: '/api',
+  jsonApiBaseURL: 'https://foia-api.ddev.site/api',
+  requestApiBaseURL: 'https://foia-api.ddev.site/api',
+  jsonApiKey: 'x-local-key-not-used',
 };
 
 export default {
-    api,
-    appEnv: 'ddev',
+  api,
+  appEnv: 'ddev',
 };
