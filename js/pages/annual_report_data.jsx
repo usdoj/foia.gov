f63fd816847fd3d3e1cc242a84911640019d02b7
brockfanning <brockfanning@gmail.com>
2021-11-09 09:21:49 -0500

    Update annual_report_data.jsx

--- js/pages/annual_report_data.jsx
+++ js/pages/annual_report_data.jsx
@@ -180,8 +180,7 @@ class AnnualReportDataPage extends Component {
               Annual Reports shine a light on agencies&apos; administration of the FOIA by
               providing members of the public with detailed FOIA statistics. Users can filter
               data by agency, data type, and fiscal year. Users can also compare data across years
-              and agencies. Annual Report data is displayed as a chart on FOIA.gov and can also
-              be downloaded as a CSV file. For full data sets from agency Annual Reports, please
+              and agencies. For full data sets from agency Annual Reports, please
               visit <a href="/developer/">the Developer page</a>.
             </p>
             <form >
