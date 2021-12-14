acb47ae634a9d74d8f96c9f95c4cf0e68ecb8c34
Matt Poole <mpoole@elevatedthird.com>
2021-12-14 17:12:32 -0500

    FE2-74 : Menu updates to test htmlproofer.

--- bin/htmlproofer.sh
+++ bin/htmlproofer.sh
@@ -4,5 +4,9 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+IGNORE_HREFS=$(ruby -e 'puts %w{
+  \/chief-foia-officers-council
+}.map{|h| "/#{h}/"}.join(",")')
+
 # Run htmlproofer with an ignore list of files
-bundle exec htmlproofer _site --check-html --disable-external --file-ignore $(paste -sd, .htmlprooferignore)
+bundle exec htmlproofer _site --check-html --disable-external --file-ignore $(paste -sd, .htmlprooferignore) --url-ignore $IGNORE_HREFS $@
