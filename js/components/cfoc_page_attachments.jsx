b34a3858effc6bc6f44bd7819d4431bf1b398a45
Matt Poole <mpoole@elevatedthird.com>
2021-12-21 14:21:46 -0500

    FE2-72: Added save-alt svg file for attachments on CFO pages.

--- js/components/cfoc_page_attachments.jsx
+++ js/components/cfoc_page_attachments.jsx
@@ -9,7 +9,10 @@ const CFOCPageAttachmentsComponent = (props) => {
         attachments.map((attachment, index) => {
           const key = index + 1;
           return (
-            <a className="usa-button usa-button-primary-alt" href={attachment.attachment_file} rel="noreferrer" target="_blank" key={key}>{attachment.attachment_title}</a>
+            <a className="usa-button usa-button-primary-alt" href={attachment.attachment_file} rel="noreferrer" target="_blank" key={key}>
+              {attachment.attachment_title}
+              <span />
+            </a>
           );
         })
       }
