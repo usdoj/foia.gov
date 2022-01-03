0aabeaef19c2fd95bd5669827edd9c62ecaa1a73
Brock Fanning <brockfanning@gmail.com>
2022-01-03 17:51:08 +0000

    Fix history import syntax

--- js/request.jsx
+++ js/request.jsx
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import { appEnv } from 'settings';
 import ConfirmationTestPage from './pages/confirmation_test';
 import AgencyComponentRequestPage from './pages/agency_component_request';
