f04a3fe00be194a7847328bfc3e15a3cb3aebc6f
Yan Chen <yan.chen@usdoj.gov>
2022-11-04 19:57:51 +0000

    FOIA-257: WS-257 Remove code from appearing on tool tips.

--- js/components/foia_tooltip.jsx
+++ js/components/foia_tooltip.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Tooltip from 'tooltip.js';
 import PropTypes from 'prop-types';
 
 class FoiaTooltip extends Component {
+  _handleOnMouseEnter = (event) => {
+    var html = event.target.getAttribute('title-data');
+    event.target.setAttribute('title', $(html).text());
+  }
+  _handleOnClick = (event) => {
+    var html = event.target.getAttribute('title-data');
+    event.target.setAttribute('title', html);
+    setTimeout($.proxy(function (e) {
+      var html = event.target.getAttribute('title-data');
+      event.target.setAttribute('title', $(html).text());
+    }, 1000), event);
+  }
   componentDidMount() {
     this.tooltip = new Tooltip(this.trigger, {
       title: this.trigger.getAttribute('data-tooltip'),
@@ -10,13 +23,20 @@ class FoiaTooltip extends Component {
       html: true,
       closeOnClickOutside: true,
     });
+    ReactDOM.findDOMNode(this).addEventListener('mouseenter', this._handleOnMouseEnter);
+    ReactDOM.findDOMNode(this).addEventListener('click', this._handleOnClick);
+  }
+  componentWillUnmount() {
+    ReactDOM.findDOMNode(this).removeEventListener('mouseenter', this._handleOnMouseEnter);
+    ReactDOM.findDOMNode(this).removeEventListener('click', this._handleOnClick);
   }
 
   render() {
     return (
-      <button type="button" className="tooltip-trigger button-as-link" ref={(trigger) => { this.trigger = trigger; }} title={this.props.text}>
+      <button type="button" className="tooltip-trigger button-as-link" ref={(trigger) => { this.trigger = trigger; }} title="" title-data={this.props.text} >
         <span className="visually-hidden">Tooltip</span>
       </button>
+
     );
   }
 }
