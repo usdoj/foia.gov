cc33e836c23c3acdec8b96ecd7f0c3ff75eabd21
Amir Meshkin <amir.meshkin@gmail.com>
2022-11-10 16:12:55 -0500

    FOIA-AWS-1637: Fixed some available fields, and function, return response, cleanup.

--- js/pages/swagger_page.jsx
+++ js/pages/swagger_page.jsx
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SwaggerUI from 'swagger-ui-react';
 import SwaggerSpec from '../../swagger.json';
 
-class SwaggerPage extends Component {
-  static handleResponse(response) {
+function handleResponse(response) {
+  if (response.status !== 200) {
     if (response.body.error.code === 'API_KEY_MISSING') {
       // eslint-disable-next-line no-alert
       alert(`API Authorization Required\n${response.body.error.message}`);
     }
   }
+  return response;
+}
 
-  render() {
-    return (
-      <div className="usa-grid">
-        <div className="usa-width-one-whole">
-          <SwaggerUI
-            responseInterceptor={SwaggerPage.handleResponse}
-            spec={SwaggerSpec}
-            displayRequestDuration
-            showExtensions
-            showCommonExtensions
-            syntaxHighlight
-            requestSnippetsEnabled
-            requestSnippets
-            withCredentials
-            defaultModelsExpandDepth={-1}
-            persistAuthorization
-            tryItOutEnabled
-            filter
-            history={this.props.history}
-            supportedSubmitMethods={['get', 'options', 'head', 'patch', 'trace']}
-          />
-        </div>
+function SwaggerPage(props) {
+  return (
+    <div className="usa-grid">
+      <div className="usa-width-one-whole">
+        <SwaggerUI
+          responseInterceptor={handleResponse}
+          spec={SwaggerSpec}
+          displayRequestDuration
+          syntaxHighlight
+          persistAuthorization
+          tryItOutEnabled
+          deepLinking
+          history={props.history}
+          supportedSubmitMethods={['get', 'options', 'head', 'patch', 'trace']}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 SwaggerPage.propTypes = {
