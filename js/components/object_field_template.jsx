088a48156e8baac7a122f7059ef1cc13e056d150
Amir Meshkin <amir.meshkin@gmail.com>
2023-03-30 13:38:36 -0400

    FOIA-292: Pushing up for a second look.

--- js/components/object_field_template.jsx
+++ js/components/object_field_template.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 // Would be nice if this template was exported so we could just wrap it without
 // having to copy/paste it.
 // https://github.com/mozilla-services/react-jsonschema-form/blob/8e9aec57dd2421f37073ec0eac917e0840dde0b3/src/components/fields/ObjectField.js#L10
-function CustomObjectFieldTemplate(ObjectFieldTemplateProps) {
-
-  const { formContext = {}} = ObjectFieldTemplateProps;
+function CustomObjectFieldTemplate(props) {
+  const { TitleField, DescriptionField, formContext = {}} = props;
+  // const { formContext = {}} = props;
   const steps = formContext.steps || [];
-  const step = steps.indexOf(ObjectFieldTemplateProps.idSchema.$id) + 1;
+  const step = steps.indexOf(props.idSchema.$id) + 1;
 
+
+  console.log("TitleField", TitleField);
+  console.log("props", props);
   return (
     <div>
       { step > 0 &&
@@ -18,18 +21,30 @@ function CustomObjectFieldTemplate(ObjectFieldTemplateProps) {
         </div>
       }
       <fieldset>
-        {(ObjectFieldTemplateProps.uiSchema["ui:title"] || ObjectFieldTemplateProps.title) && (
-          <legend id={`${ObjectFieldTemplateProps.idSchema.$id}__title`}>{ObjectFieldTemplateProps.title}</legend>
+        {(props.uiSchema["ui:title"] || props.title) && (
+          // <legend id={`${props.idSchema.$id}__title`}>{props.title}</legend>
+          // {titleField}
+          <TitleField
+          id={`${props.idSchema.$id}__title`}
+          title={props.title || props.uiSchema["ui:title"]}
+          required={props.required}
+          formContext={props.formContext}
+          />
         )}
 
-        {ObjectFieldTemplateProps.description && (
-          <div id={`${ObjectFieldTemplateProps.idSchema.$id}__description`} className="field-description">
+        {props.description && (
+          <div id={`${props.idSchema.$id}__description`} className="field-description">
             <span>
-              <p>{ObjectFieldTemplateProps.description}</p>
+              <p>{props.description}</p>
             </span>
           </div>
+          // <DescriptionField
+          // id={`${props.idSchema.$id}__description`}
+          // description={props.description}
+          // formContext={props.formContext}
+          // />
         )}
-        {ObjectFieldTemplateProps.properties.map(prop => prop.content)}
+        {props.properties.map(prop => prop.content)}
         <div className="foia-request-form_nav-top">
           <a href="#main">Return to top</a>
         </div>
