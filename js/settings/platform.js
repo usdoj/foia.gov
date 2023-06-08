c5602511d48c65ec07af00d17c0f320c81ec4141
Matt West <mwest@forumone.com>
2023-06-08 12:37:29 -0400

    Added platform JS environment.

---
+++ js/settings/platform.js
@@ -0,0 +1,11 @@
+const api = {
+  localApiBaseURL: '/api',
+  jsonApiBaseURL: 'https://api.main-bvxea6i-oafzps2pqxjxw.us-2.platformsh.site/api',
+  requestApiBaseURL: 'https://api.main-bvxea6i-oafzps2pqxjxw.us-2.platformsh.site/api',
+  jsonApiKey: 'x-local-key-not-used',
+};
+
+export default {
+  api,
+  appEnv: 'ddev',
+};
