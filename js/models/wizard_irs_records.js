8a07e6b9eae9691b4b5bbb1e0223e404e2c65a9e
Donna Hogan <ddhogan@users.noreply.github.com>
2023-06-21 09:43:14 -0400

    [FOIA22-87] Encode User Flow: IRS Records

--- js/models/wizard_irs_records.js
+++ js/models/wizard_irs_records.js
@@ -1,10 +1,47 @@
 /**
  * @type {WizardQuestion}
  */
+const irsTypeQuestion = {
+  type: 'question',
+  titleMid: 'q3',
+  answers: [
+    {
+      titleMid: 'a15',
+      next: { type: 'summary', titleMid: 'm16' },
+    },
+    {
+      titleMid: 'a16',
+      next: { type: 'summary', titleMid: 'm17' },
+    },
+    {
+      titleMid: 'a17',
+      next: { type: 'summary', titleMid: 'm18' },
+    },
+  ],
+};
+
+/** @type {WizardQuestion} */
 const irsRecordsJourney = {
   type: 'question',
   titleMid: 'q1',
-  answers: [],
+  answers: [
+    {
+      titleMid: 'a1',
+      next: {
+        type: 'continue',
+        titleMid: 'm1',
+        next: irsTypeQuestion,
+      },
+    },
+    {
+      titleMid: 'a2',
+      next: {
+        type: 'continue',
+        titleMid: 'm2',
+        next: irsTypeQuestion,
+      },
+    },
+  ],
 };
 
 export default irsRecordsJourney;
