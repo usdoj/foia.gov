51fb986ce6ff2a4c7f8d987a8f640ea71faccdf2
Donna Hogan <ddhogan@users.noreply.github.com>
2023-06-21 12:07:00 -0400

    [FOIA22-88] User Flow: SocSec records

--- js/models/wizard_socsec_records.js
+++ js/models/wizard_socsec_records.js
@@ -4,7 +4,44 @@
 const socsecRecordsJourney = {
   type: 'question',
   titleMid: 'q1',
-  answers: [],
+  answers: [
+    {
+      titleMid: 'a1',
+      next: {
+        type: 'continue',
+        titleMid: 'm1',
+        next: {
+          type: 'summary',
+          titleMid: 'm19',
+        },
+      },
+    },
+    {
+      titleMid: 'a2',
+      next: {
+        type: 'continue',
+        titleMid: 'm2',
+        next: {
+          type: 'question',
+          titleMid: 'q4',
+          answers: [
+            {
+              titleMid: 'a18-1',
+              next: { type: 'summary', titleMid: 'm20' },
+            },
+            {
+              titleMid: 'a18-2',
+              next: { type: 'summary', titleMid: 'm21' },
+            },
+            {
+              titleMid: 'a18-3',
+              next: { type: 'summary', titleMid: 'm22' },
+            },
+          ],
+        },
+      },
+    },
+  ],
 };
 
 export default socsecRecordsJourney;
