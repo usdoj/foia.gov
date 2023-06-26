be06ee2a678478bc5b29db0b52fc451dab3b2b66
Steve Clay <steve@mrclay.org>
2023-06-26 16:25:41 -0400

    [FOIA22-15] Ready landing page for agency details view

--- js/components/landing.jsx
+++ js/components/landing.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { requestActions } from 'actions';
@@ -6,167 +6,161 @@ import AgencyComponentPreview from 'components/agency_component_preview';
 import AgencyPreview from 'components/agency_preview';
 import agencyComponentStore from '../stores/agency_component';
 
-function LandingComponent({
-  agencyFinderDataComplete,
-  onChangeUrlQueryParams,
-  idQueryString = null,
-  typeQueryString = null,
-}) {
-  const [showHome, setShowHome] = useState(typeQueryString === null);
-  const [agency, setAgency] = useState(null);
-  const [agencyComponent, setAgencyComponent] = useState(null);
-  const [agencyComponentsForAgency, setAgencyComponentsForAgency] = useState(null);
-  const [isCentralized, setIsCentralized] = useState(false);
+class LandingComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      agency: null,
+      agencyComponent: null,
+      agencyComponentsForAgency: null,
+      showHome: props.typeQueryString === null,
+    };
+  }
 
-  useEffect(() => {
-    const method = showHome ? 'show' : 'hide';
+  syncHomeContentVisibility() {
+    const method = this.state.showHome ? 'show' : 'hide';
     $('#main > .usa-hero, #learn-more')[method]();
-  }, [showHome]);
+  }
 
-  // Store state without re-rendering.
-  const fakeThis = useRef({
-    queryStringConsulted: false,
-  }).current;
+  componentDidMount() {
+    this.syncHomeContentVisibility();
+  }
 
-  function setStateForAgency(newAgency, newAgencyComponentsForAgency) {
-    setAgency(newAgency);
-    setAgencyComponent(null);
-    setAgencyComponentsForAgency(newAgencyComponentsForAgency);
+  componentDidUpdate() {
+    this.syncHomeContentVisibility();
 
-    onChangeUrlQueryParams({
-      idQueryString: newAgency.id,
+    if (!this.props.agencyFinderDataComplete) {
+      return;
+    }
+    this.consultQueryString();
+  }
+
+  setStateForAgency(agency, agencyComponentsForAgency) {
+    this.setState({
+      agency,
+      agencyComponent: null,
+      agencyComponentsForAgency,
+    });
+    this.props.onChangeUrlQueryParams({
+      idQueryString: agency.id,
       typeQueryString: 'agency',
     });
   }
 
-  function setStateForComponent(newAgencyComponent, newIsCentralized = false) {
-    setAgency(null);
-    setAgencyComponent(newAgencyComponent);
-    setAgencyComponentsForAgency(null);
-    setIsCentralized(newIsCentralized);
-
-    onChangeUrlQueryParams({
-      idQueryString: newAgencyComponent.id,
+  setStateForComponent(agencyComponent, isCentralized = false) {
+    this.setState({
+      agency: null,
+      agencyComponent,
+      agencyComponentsForAgency: null,
+      isCentralized,
+    });
+    this.props.onChangeUrlQueryParams({
+      idQueryString: agencyComponent.id,
       typeQueryString: 'component',
     });
   }
 
-  function consultQueryString() {
+  consultQueryString() {
     // We only want to do this one time.
-    if (fakeThis.queryStringConsulted) {
+    if (this.queryStringConsulted) {
       return;
     }
-    fakeThis.queryStringConsulted = true;
+    this.queryStringConsulted = true;
+
+    const {
+      typeQueryString,
+      idQueryString,
+    } = this.props;
 
     if (typeQueryString === 'agency') {
-      const newAgency = agencyComponentStore.getAgency(idQueryString);
-      const newAgencyComponentsForAgency = agencyComponentStore.getAgencyComponentsForAgency(newAgency.id);
-      setStateForAgency(newAgency, newAgencyComponentsForAgency);
-      setShowHome(false);
+      const agency = agencyComponentStore.getAgency(idQueryString);
+      const agencyComponentsForAgency = agencyComponentStore.getAgencyComponentsForAgency(agency.id);
+      this.setStateForAgency(agency, agencyComponentsForAgency);
     }
     if (typeQueryString === 'component') {
       const component = agencyComponentStore.getAgencyComponent(idQueryString);
-      const newAgency = agencyComponentStore.getAgency(component.agency.id);
-      setStateForComponent(component, newAgency.isCentralized());
-      setShowHome(false);
+      const agency = agencyComponentStore.getAgency(component.agency.id);
+      this.setStateForComponent(component, agency.isCentralized());
     }
   }
 
-  useEffect(() => {
-    if (!agencyFinderDataComplete) {
-      return;
-    }
-
-    consultQueryString();
-  }, [agencyFinderDataComplete]);
-
-  // Note that the agencyComponent comes from two different sources, so the
-  // properties might not be consistent.
-  const agencyChange = (newAgencyComponent) => {
-    function fetchAgencyComponent(agencyComponentId) {
-      return requestActions.fetchAgencyComponent(agencyComponentId)
-        .then(requestActions.receiveAgencyComponent)
-        .then(() => agencyComponentStore.getAgencyComponent(agencyComponentId));
-    }
-
-    setShowHome(false);
-
-    if (newAgencyComponent.type === 'agency_component') {
-      fetchAgencyComponent(newAgencyComponent.id)
-        .then((component) => setStateForComponent(component, false));
-      return;
-    }
-
-    const newAgency = agencyComponentStore.getAgency(newAgencyComponent.id);
-
-    // Treat centralized agencies as components
-    if (newAgency.isCentralized()) {
-      const component = agencyComponentStore
-        .getState()
-        .agencyComponents
-        .find((c) => c.agency.id === newAgency.id);
-      fetchAgencyComponent(component.id)
-        .then((c) => setStateForComponent(c, true));
-      return;
-    }
-
-    const newAgencyComponentsForAgency = agencyComponentStore.getAgencyComponentsForAgency(newAgency.id);
-    setStateForAgency(agency, newAgencyComponentsForAgency);
-  };
-
-  if (showHome) {
-    return null;
-  }
-
-  // TODO the old styles were tied to this class. Still needed?
-  // <section className="agency-component-search" id="agency-search">
-
-  return (
-    <div className="usa-grid">
-      <p>
-        <a href="/agency-search.html">&lt; Agency Search</a>
-      </p>
-
-      {
-        agencyComponent
-        && (
-          <AgencyComponentPreview
-            agencyComponent={agencyComponent.toJS()}
-            isCentralized={isCentralized}
-            onAgencySelect={agencyChange}
-          />
-        )
+  render() {
+    // Note that the agencyComponent comes from two different sources, so the
+    // properties might not be consistent.
+    const agencyChange = (agencyComponent) => {
+      function fetchAgencyComponent(agencyComponentId) {
+        return requestActions.fetchAgencyComponent(agencyComponentId)
+          .then(requestActions.receiveAgencyComponent)
+          .then(() => agencyComponentStore.getAgencyComponent(agencyComponentId));
       }
-      {
-        agency
-        && (
-          <AgencyPreview
-            agency={agency}
-            agencyComponentsForAgency={agencyComponentsForAgency}
-            onAgencySelect={agencyChange}
-          />
-        )
+
+      if (agencyComponent.type === 'agency_component') {
+        fetchAgencyComponent(agencyComponent.id)
+          .then((component) => this.setStateForComponent(component, false));
+        return;
       }
-      {
-        !agencyComponent && !agency
-        && (
-          <div>
-            <h3 className="agency-component-search_hed">When choosing an agency</h3>
-            <p>
-              Remember that some agencies can’t yet receive FOIA requests
-              through FOIA.gov. For those agencies, this site   will provide
-              you with the information you need to submit a request directly to
-              the agency.
-            </p>
+
+      const agency = agencyComponentStore.getAgency(agencyComponent.id);
+
+      // Treat centralized agencies as components
+      if (agency.isCentralized()) {
+        const component = agencyComponentStore
+          .getState()
+          .agencyComponents
+          .find((c) => c.agency.id === agency.id);
+        fetchAgencyComponent(component.id)
+          .then((c) => this.setStateForComponent(c, true));
+        return;
+      }
+
+      const agencyComponentsForAgency = agencyComponentStore.getAgencyComponentsForAgency(agency.id);
+      this.setStateForAgency(agency, agencyComponentsForAgency);
+    };
+
+    const { agencyFinderDataComplete, agencyFinderDataProgress } = this.props;
+
+    return (
+      <div className="usa-grid agency-preview">
+        <p>
+          <a href="/agency-search.html">&lt; Agency Search</a>
+        </p>
+
+        {!agencyFinderDataComplete && (
+          <div className="foia-component-agency-search__loading">
+            Loading progress:
+            {agencyFinderDataProgress}
+            %
           </div>
-        )
-      }
-    </div>
-  );
+        )}
+
+        {
+          this.state.agencyComponent
+          && (
+            <AgencyComponentPreview
+              agencyComponent={this.state.agencyComponent.toJS()}
+              isCentralized={this.state.isCentralized}
+              onAgencySelect={agencyChange}
+            />
+          )
+        }
+        {
+          this.state.agency
+          && (
+            <AgencyPreview
+              agency={this.state.agency}
+              agencyComponentsForAgency={this.state.agencyComponentsForAgency}
+              onAgencySelect={agencyChange}
+            />
+          )
+        }
+      </div>
+    );
+  }
 }
 
 LandingComponent.propTypes = {
   agencyFinderDataComplete: PropTypes.bool.isRequired,
+  agencyFinderDataProgress: PropTypes.number.isRequired,
   onChangeUrlQueryParams: PropTypes.func.isRequired,
   idQueryString: PropTypes.string,
   typeQueryString: PropTypes.string,
