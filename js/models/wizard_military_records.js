ba8db5272703783b1f323fdf61ae4536e3f05d24
Steve Clay <steve@mrclay.org>
2023-06-30 13:05:01 -0400

    [FOIA22-91] Military records

--- js/models/wizard_military_records.js
+++ js/models/wizard_military_records.js
@@ -21,25 +21,31 @@ const militaryRecordsJourney = question('q10', [
     yesNoQuestion('q1', {
       yes: continueStep(
         'm1',
-        question('q12', [
-          answer('literal:OMPF', summary('m39')),
-          answer('literal:Medical Records', summary('m25')),
-          answer('literal:DD Form 214 (Report of Separation)', summary('m40')),
-          answer("literal:Veteran's Benefits", summary('m41')),
-          answer('literal:National Guard', summary('m42')),
-          answer('literal:None of the above', summary('TBD')),
-        ]),
+        question(
+          'q12',
+          [
+            answer('literal:OMPF', summary('m39')),
+            answer('literal:Medical Records', summary('m25')),
+            answer('literal:DD Form 214 (Report of Separation)', summary('m40')),
+            answer("literal:Veteran's Benefits", summary('m41')),
+            answer('literal:National Guard', summary('m42')),
+          ],
+          'literal:<p>For other information, we suggest you contact your medical provider directly.</p>',
+        ),
       ),
       no: continueStep(
         'm2',
-        question('q12', [
-          answer('literal:OMPF', summary('m43')),
-          answer('literal:Medical Records', summary('m27')),
-          answer('literal:DD Form 214 (Report of Separation)', summary('m44')),
-          answer("literal:Veteran's Benefits", summary('m45')),
-          answer('literal:National Guard', summary('m46')),
-          answer('literal:None of the above', summary('TBD')),
-        ]),
+        question(
+          'q12',
+          [
+            answer('literal:OMPF', summary('m43')),
+            answer('literal:Medical Records', summary('m27')),
+            answer('literal:DD Form 214 (Report of Separation)', summary('m44')),
+            answer("literal:Veteran's Benefits", summary('m45')),
+            answer('literal:National Guard', summary('m46')),
+          ],
+          'literal:<p>For other information, we suggest contacting the medical provider directly.</p>',
+        ),
       ),
     }),
   ),
