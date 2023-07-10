9f38e5ee77e5a9497b4cf218901e9e01679c713a
Steve Clay <steve@mrclay.org>
2023-07-10 16:39:13 -0400

    Node-safe urlParams

--- js/components/wizard_page_summary.jsx
+++ js/components/wizard_page_summary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useWizard } from '../stores/wizard_store';
+import { urlParams } from '../util/wizard_helpers';
 import PageTemplate from './wizard_template_page';
 import Constrain from './wizard_layout_constrain';
 import Heading from './wizard_component_heading';
@@ -11,7 +12,7 @@ import NoResults from './wizard_component_no_results';
 import RichText from './wizard_component_rich_text';
 import WizardHtml from './wizard_html';
 
-const limit = parseInt(new URLSearchParams(location.search).get('limit') || '6', 10);
+const limit = parseInt(urlParams().get('limit') || '6', 10);
 
 function Summary() {
   const {
