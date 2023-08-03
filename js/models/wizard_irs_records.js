6e57f35e1f7cab5eed43589d8d262f6ad02005b2
Steve Clay <steve@mrclay.org>
2023-08-03 19:42:32 -0400

    [FOIA22-114] Content for IRS

--- js/models/wizard_irs_records.js
+++ js/models/wizard_irs_records.js
@@ -1,54 +1,39 @@
 import extraMessages from './wizard_extra_messages';
+import {
+  answer, continueStep, question, summary, yesNoQuestion,
+} from '../util/wizard_helpers';
 
 /**
  * @type {WizardQuestion}
  */
-const irsTypeQuestion = {
-  type: 'question',
-  titleMid: 'q3',
-  answers: [
-    {
-      titleMid: 'a15',
-      newDisplayedTopic: extraMessages.a15,
-      next: { type: 'summary', titleMid: 'm16' },
-    },
-    {
-      titleMid: 'a16',
-      newDisplayedTopic: extraMessages.a16,
-      next: { type: 'summary', titleMid: 'm17' },
-    },
-    {
-      titleMid: 'a17',
-      newDisplayedTopic: extraMessages.a17,
-      next: { type: 'summary', titleMid: 'm18' },
-    },
-  ],
-};
+const irsTypeQuestion = question('q1', [
+  answer(
+    'a15',
+    summary('m16'),
+    extraMessages.a15,
+  ),
+  answer(
+    'a16',
+    summary('m17'),
+    extraMessages.a16,
+  ),
+  answer(
+    'a17',
+    summary('m18'),
+    extraMessages.a17,
+  ),
+  answer(
+    'literal:None of the Above (I want to start over)',
+    { type: 'start-over' },
+  ),
+]);
 
 /** @type {WizardQuestion} */
-const irsRecordsJourney = {
-  type: 'question',
-  titleMid: 'q1',
-  answers: [
-    {
-      titleMid: 'a1',
-      newDisplayedTopic: 'Your own IRS records',
-      next: {
-        type: 'continue',
-        titleMid: 'm1',
-        next: irsTypeQuestion,
-      },
-    },
-    {
-      titleMid: 'a2',
-      newDisplayedTopic: 'Someone else\'s IRS records',
-      next: {
-        type: 'continue',
-        titleMid: 'm2',
-        next: irsTypeQuestion,
-      },
-    },
-  ],
-};
+const irsRecordsJourney = yesNoQuestion('q1', {
+  topicIfYes: 'Your own Internal Revenue Service (IRS) records',
+  topicIfNo: 'Someone else’s Internal Revenue Service (IRS) records',
+  yes: continueStep('m1', irsTypeQuestion),
+  no: continueStep('m2', irsTypeQuestion),
+});
 
 export default irsRecordsJourney;
