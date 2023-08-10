6d8f993789255abeb7304c124795fd37c38d28f3
Steve Clay <steve@mrclay.org>
2023-08-10 12:32:35 -0400

    Proxy Polydelta requests through uat-api.foia.gov/doj-foia

--- js/settings/uat.js
+++ js/settings/uat.js
@@ -2,6 +2,7 @@ const api = {
   localApiBaseURL: '/api',
   jsonApiBaseURL: 'https://uat-api.foia.gov/api',
   requestApiBaseURL: 'https://uat-api.foia.gov/api',
+  wizardApiURL: 'https://uat-api.foia.gov/doj-foia/models/ZBA2ow0/predict',
   // These are not secret, refer to https://github.com/18F/beta.foia.gov/tree/develop/docs/foia-api.md
   jsonApiKey: 'PvO2Wh2UppBQqGIxIvdKbogNRI0W0MVLFhkxjWfK',
 };
--- js/util/wizard_api.js
+++ js/util/wizard_api.js
@@ -13,11 +13,11 @@ export function fetchWizardPredictions(query) {
   const polydeltaOptions = {
     method: 'POST',
     headers: {
-      Authorization: 'Api-Key RQmvMuDs.vS8ziJy9FeFlrxUoAji4or840T576esG',
+      'X-Api-Key': settings.api.jsonApiKey,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ input: [query] }),
   };
-  return fetch('https://app.polydelta.ai/doj-foia/models/ZBA2ow0/predict', polydeltaOptions)
+  return fetch(settings.api.wizardApiURL, polydeltaOptions)
     .then((response) => response.json());
 }
