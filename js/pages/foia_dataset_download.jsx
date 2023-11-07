47a26d763f0cab0a66ed3e35ceaf378cefbd9bdd
Amir Meshkin <amir.meshkin@gmail.com>
2023-11-07 11:51:18 -0500

    FOIA-403: Verbiage update.

--- js/pages/foia_dataset_download.jsx
+++ js/pages/foia_dataset_download.jsx
@@ -7,7 +7,7 @@ function FoiaDatasetDownload() {
     <div className="usa-grid">
       <div className="usa-width-one-whole">
         <h1>FOIA Data Set Downloads</h1>
-        <p>The following compressed files are complete archives of the agency FOIA data sets for the years 2008 through 2022 in XML format.</p>
+        <p>The following compressed files are complete archives of the agency FOIA data sets for the years 2008 through 2022 in .xml format.</p>
         <ul>
           <li><a href="/2022-FOIASetFull.zip">Download the full 2022 FOIA Data set</a> (.zip format)</li>
           <li><a href="/2021-FOIASetFull.zip">Download the full 2021 FOIA Data set</a> (.zip format)</li>
@@ -27,7 +27,7 @@ function FoiaDatasetDownload() {
         </ul>
       </div>
       <div className="usa-width-one-whole pt5">
-        <p>The following compressed files are complete archives of the agency FOIA data sets for the years 2008 through 2022 in CSV format.</p>
+        <p>The following compressed files are complete archives of governmentwide FOIA data sets for the years 2008 through 2022 in .csv format.</p>
         <ul>
           <li><a href="/downloads/all_agencies_csv_2022.zip">Download the full 2022 All Agency Data set</a> (.zip format)</li>
           <li><a href="/downloads/all_agencies_csv_2021.zip">Download the full 2021 All Agency Data set</a> (.zip format)</li>
