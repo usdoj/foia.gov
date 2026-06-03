1620ea26253712ad41bb96ff37b0bf075c7cd466
Brock Fanning <1319083+brockfanning@users.noreply.github.com>
2026-06-03 17:31:09 +0000

    FOIA-545: Remove debugging.

--- js/stores/agency_component.js
+++ js/stores/agency_component.js
@@ -80,11 +80,9 @@ class AgencyComponentStore extends Store {
         // Count up the agency's num_requests total from components.
         let numRequests = 0;
         this.getAgencyComponentsForAgency(agency.id).forEach((component) => {
-          console.log('Component - ' + component.title + ' num_requests: ' + component.num_requests);
           numRequests += parseInt(component?.num_requests, 10) || 0;
         });
         // Add a title and num_requests property for common displayKey
-        console.log('Agency - ' + agency.name + ' num_requests: ' + numRequests);
         return {
           ...agency.toJS(),
           title: agency.name,
